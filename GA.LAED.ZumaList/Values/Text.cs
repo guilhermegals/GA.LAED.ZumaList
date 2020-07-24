fad8f612228e3e224c083a5da6edb672d53ba423
Guilherme Leite <guilhermegals@gmail.com>
2020-07-24 20:41:00 -0300

    Language game change

--- GA.LAED.ZumaList/Values/Text.cs
+++ GA.LAED.ZumaList/Values/Text.cs
@@ -1,35 +1,42 @@
-﻿namespace GA.LAED.ZumaList.Values {
-    public static class Text {
-        public class GameText {
+﻿namespace GA.LAED.ZumaList.Values
+{
+    public static class Text
+    {
+        public class GameText
+        {
             public const string TITLE = "Zuma Game";
             public const string LINE = "------------------";
-            public const string WRITE = "Digite: ";
-            public const string BYE = "Obrigado por jogar!";
-            public const string CREDITS = "Desenvolvido por Daniel dos Reis Braz & Guilherme Augusto Leite Santos";
-            public const string ENTER = "Enter para continuar...";
+            public const string WRITE = "Type: ";
+            public const string BYE = "Thanks for Playing!";
+            public const string CREDITS = "Developed by Daniel dos Reis Braz & Guilherme Augusto Leite Santos";
+            public const string ENTER = "Enter...";
         }
 
-        public class BallText {
+        public class BallText
+        {
             public const string BALL = "O";
         }
 
-        public class MenuText {
-            public const string PLAY = "Jogar";
-            public const string EXIT = "Sair";
-            public const string CREDITS = "Créditos";
+        public class MenuText
+        {
+            public const string PLAY = "Play";
+            public const string EXIT = "Exit";
+            public const string CREDITS = "Credits";
         }
 
-        public class InGameText {
-            public const string LIST_BALL = "Bolas: ";
-            public const string PLAYER_LIST_BALL = "Bolas disponíveis: ";
-            public const string SELECT_BALL = "Qual bola disponível você deseja inserir? ";
-            public const string SELECT_POSITION = "Em qual posição você deseja inserir? ";
-            public const string SCORE = "Pontos: ";
+        public class InGameText
+        {
+            public const string LIST_BALL = "Balls: ";
+            public const string PLAYER_LIST_BALL = "Available balls: ";
+            public const string SELECT_BALL = "What available ball you wanna insert? ";
+            public const string SELECT_POSITION = "Which position you wanna insert? ";
+            public const string SCORE = "Score: ";
         }
 
-        public class GameOverText {
-            public const string GAME_OVER = "Fim de jogo!";
-            public const string FINAL_SCORE = "Pontuação final: ";
+        public class GameOverText
+        {
+            public const string GAME_OVER = "Game over!";
+            public const string FINAL_SCORE = "Final score: ";
         }
     }
 }
